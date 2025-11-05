5d4fa6e8516f258239173e0dc6d4adc2cd30dfd3
lry <1937552355@qq.com>
2025-11-05 22:30:43 +0800

    add some annotation

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import { StrictMode } from 'react'
+import { StrictMode } from 'react' //StrictMode严格模式
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+//渲染root
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react'; //调用vite提供的react插件
 
 export default defineConfig({
   plugins: [react()],
